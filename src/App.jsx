d3da790dc67e5e33a9ae143b0565a43ed77e52f3
arafatah <arafathowlader890@gmail.com>
2025-04-07 15:57:22 +0600

    feat: refactor App component layout and enhance Row component for flexible alignment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Heading from "./ui/Heading";
 import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -15,20 +15,24 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row>
-          <Heading as="h1">The wild Oasis</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
 
-          <div>
-            <Heading as="h2">Check In and Out</Heading>
-            <Button onClick={() => alert("Check in")}>Check In</Button>
-            <Button onClick={() => alert("Check out")}>Check Out</Button>{" "}
-          </div>
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Button onClick={() => alert("Check in")}>Check In</Button>
+              <Button onClick={() => alert("Check out")}>Check Out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
-        <Heading as="h3">Form</Heading>
-        <form>
-          <Input type="number" placeholder="Number of guests" />
-          <Input type="number" placeholder="Number of guests" />
-        </form>{" "}
       </StyledApp>
     </>
   );
