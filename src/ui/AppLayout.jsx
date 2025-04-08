387d6d191e6ef576281cc893e8ef573517ae0902
arafatah <arafathowlader890@gmail.com>
2025-04-08 14:53:50 +0600

    Refactor code structure for improved readability and maintainability

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,12 +8,10 @@ const StyledAppLayout = styled.div`
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
-
-  
 `;
 
 const Main = styled.main`
-  background-color: green;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 
