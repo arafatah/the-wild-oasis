37fd436f4067b13dfbe2e13fded906870e5fd93c
arafatah <arafathowlader890@gmail.com>
2025-04-09 12:59:15 +0600

    feat: implement CabinRow and integrate with CabinTable for displaying cabin data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -38,3 +38,14 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+
+function CabinRow() {
+  return (
+    <div>
+      Row
+    </div>
+  )
+}
+
+export default CabinRow
