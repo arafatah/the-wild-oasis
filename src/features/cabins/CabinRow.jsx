2d72a4a5befead92be76109097e8012147cd0fc6
arafatah <arafathowlader890@gmail.com>
2025-04-10 11:17:57 +0600

    fix: update query key from "cabin" to "cabins" in CabinTable and add queryClient to invalidate queries on cabin deletion in CabinRow

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
@@ -52,8 +52,15 @@ function CabinRow({ cabin }) {
     image,
   } = cabin;
 
+  const queryClient = useQueryClient();
+
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
   });
 
   return (
