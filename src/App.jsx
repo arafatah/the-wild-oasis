ad8108c554a45e02d5d543d5a3b4467f0f05c22c
MD. Arafat Howlader <47807293+arafatah@users.noreply.github.com>
2025-04-14 12:04:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-          
+            
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
